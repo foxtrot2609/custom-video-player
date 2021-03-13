191b0d0130697dee9f1269b77bb89d96b82eff73
foxtrot2609 <73159713+foxtrot2609@users.noreply.github.com>
2021-03-13 15:38:41 +0300

    feat: add css styles

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* variables */
 let mousedown = false;
 let fullScreenFlag = false;
+let x, y;
 
 /* get elements */
 const player = document.querySelector(".player");
@@ -12,15 +13,17 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const openButton = player.querySelector("[data-action]");
 const ranges = player.querySelectorAll(".player__slider");
+const header = player.querySelector(".header");
+const playerControls = player.querySelector(".player__controls");
 
 /* functions */
 const togglePlay = () => {
   if (video.paused) {
     video.play();
-    toggle.innerHTML = "&#10074;&#10074;";
+    toggle.innerHTML = '<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xlink:href="#ytp-id-118"></use><path class="ytp-svg-fill" d="M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z" id="ytp-id-118"></path></svg>';
   } else {
     video.pause();
-    toggle.innerHTML = "&#9658;";
+    toggle.innerHTML = '<svg version="1.1" height="100%" viewBox="0 0 48 48" width="100%" xmlns="http://www.w3.org/2000/svg"><path data-icon="icon" d="M-838-2232H562v3600H-838z" fill="none" /><path d="M16 10v28l22-14z" /><path d="M0 0h48v48H0z" fill="none" /></svg>';
   }
 };
 
@@ -87,14 +90,24 @@ progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
 openButton.addEventListener("click", openFullScreen);
 
+player.addEventListener("mousemove", (e) => {
+  /* work only with fullscreen */
+  if (!fullScreenFlag) {
+    document.documentElement.style.cursor = "auto";
+    header.style.top = "";
+    playerControls.style.bottom = "";
+    return;
+  }
+  /* hide cursor and panels after 5 sec without moving mouse */
+  [x, y] = [e.screenX, e.screenY];
+  document.documentElement.style.cursor = "auto";
+  header.style.top = "";
+  playerControls.style.bottom = "";
 
-// player.addEventListener("mousemove", (e) => {
-//   if (!fullScreenFlag) {
-//     document.documentElement.style.cursor = "auto";
-//     return;
-//   }
-
-//   document.documentElement.style.cursor = "auto";
-  
-//   console.log(e)
-// });
+  setTimeout(() => {
+    if (!(x === e.screenX && y === e.screenY)) return;
+    document.documentElement.style.cursor = "none";
+    header.style.top = "-5vh";
+    playerControls.style.bottom = "calc(-6vh + 5px)";
+  }, 5000);
+});
